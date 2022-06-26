d46da68fce9e4aa2974d16318012420e59cc315f
max <maximilianwu0828@gmail.com>
2022-06-26 12:58:32 -0700

    stats and graph updates based on button inputs

--- js/backtester.js
+++ js/backtester.js
@@ -1,61 +1,124 @@
 window.onload = function () {
-  chart.render();
+  renderChart();
 }
 
 let initial = 1000;
+let current = initial;
 let numOfTrades = 0;
-let points = [
-  {x: 0, y: 1000},
-  {x: 1, y: 1026},
-  {x: 2, y: 1015},
-  {x: 3, y: 1049},
-  {x: 4, y: 1030},
-  {x: 5, y: 1300},
-  {x: 6, y: 1200},
-  {x: 7, y: 1350},
-  {x: 8, y: 1300},
-  {x: 9, y: 1367},
-  {x: 10, y: 1500},
-]
+let points = [{x: 0, y: initial}];
+let wins = 0;
+let losses = 0;
 
-var chart = new CanvasJS.Chart("chart", {
-  animationEnabled: true,
-  backgroundColor: "#202a2e",
-  axisY: {
-    minimum: initial,
-    labelFontColor: "#8c9597",
-    labelFontFamily: "Montserrat",
-    labelFontSize: 15,
-    valueFormatString: "#",
-  },
-  axisX: {
-    minimum: 0,
-    maximum: points.length - 1,
-    gridThickness: 1,
-    labelFontColor: "#8c9597",
-    labelFontFamily: "Montserrat",
-    labelFontSize: 15,
-    valueFormatString: "0",
-  },
-  data: [{
-    markerSize: 0,
-    fillOpacity: 0.5,
-    lineThickness: 3,
-    type: "splineArea",
-    dataPoints: points
-  }]
-});
-
-let value = document.getElementById("value");
-let profit = document.getElementById("profit");
-let loss = document.getElementById("loss");
+let initialValue = document.getElementById("value");
+let positive = document.getElementById("positive");
+let negative = document.getElementById("negative");
 
 let valueButton = document.getElementById("value-button");
 let profitButton = document.getElementById("profit-button");
 let lossButton = document.getElementById("loss-button");
+
+let reset = document.getElementById("reset-button");
 
 let winrate = document.getElementById("winrate");
 let pnl = document.getElementById("pnl");
 let trades = document.getElementById("trades");
 let wl = document.getElementById("wl");
 
+valueButton.addEventListener("click", () => {
+  resetStats();
+})
+
+profitButton.addEventListener("click", () => {
+  numOfTrades += 1;
+  current += parseInt(positive.value);
+  points.push({x: numOfTrades, y: current});
+  wins += 1;
+  updateStats();
+  renderChart();
+})
+
+lossButton.addEventListener("click", () => {
+  numOfTrades += 1;
+  current -= parseInt(negative.value);
+  points.push({x: numOfTrades, y: current});
+  losses += 1;
+  updateStats();
+  renderChart();
+})
+
+reset.addEventListener("click", () => {
+  resetStats();
+  initialValue.value = 1000;
+  positive.value = 3;
+  negative.value = 2;
+})
+
+let resetStats = () => {
+  initial = parseInt(initialValue.value);
+  points = [{x: 0, y: initial}];
+  numOfTrades = 0;
+  current = initial;
+  wins = 0;
+  losses = 0;
+  renderChart();
+}
+
+let renderChart = () => {
+  var chart = new CanvasJS.Chart("chart", {
+    animationEnabled: true,
+    backgroundColor: "#202a2e",
+    axisY: {
+      minimum: initial,
+      maximum: findMaxY(points),
+      labelFontColor: "#8c9597",
+      labelFontFamily: "Montserrat",
+      labelFontSize: 15,
+      valueFormatString: "#",
+    },
+    axisX: {
+      minimum: 0,
+      maximum: points.length - 1,
+      gridThickness: 1,
+      labelFontColor: "#8c9597",
+      labelFontFamily: "Montserrat",
+      labelFontSize: 15,
+      valueFormatString: "0",
+    },
+    data: [{
+      markerSize: 0,
+      fillOpacity: 0.5,
+      lineThickness: 3,
+      type: "splineArea",
+      dataPoints: points
+    }]
+  });
+  chart.render();
+}
+
+let findMaxY = (dataPoints) => {
+  max = 0;
+  for (let index = 0; index < dataPoints.length; index++) {
+    const element = dataPoints[index];
+    if(element["y"] > max) {
+      max = element["y"];
+    }
+  }
+  return max;
+}
+
+let updateStats = () => {
+  winrate.innerHTML = ((wins/numOfTrades) * 100).toFixed(2);
+  profits = (((current - initial) / initial) * 100).toFixed(2);
+
+  if(profits >= 0) {
+    pnl.innerHTML = "↑ " + profits + "%";
+    pnl.style.color = "#449a84";
+  }
+  else {
+    pnl.innerHTML = "↓ " + profits + "%";
+    pnl.style.color = "#772f34"
+  }
+
+  trades.innerHTML = numOfTrades;
+  wl.innerHTML = wins + "W / " + losses + "L"
+}
